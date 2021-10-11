6df8bcce945e0b32234176541d267b754aef1d17
goncanur uz <goncanuruz@outlook.com>
2021-10-11 23:09:26 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1 ? cevap 30
+            //int, decimal, float, doble, bool (0-1) = değer tip
+            //bellekte stack ve heap diye iki alan vardır. değer tip olanlar stackte depolanır ve sayi1 = 10'u sayi1 in değeri 10'dur olarak okur.
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0] ? cevap 999
+
+
+            //array, class, interface = referas tip
+            // sayilar1 olarak stackte tanımlanır "=new" dediğinde heapte [10,20,30] olarak tutar.
+            //sayiler1 stackte "101 (referans sayısı)" olarak tutulur [10,20,30] ise heapte "101 (referans sayısı)" olarak tutulur.
+            //daha sonrasında sayiler2 stackte "102 (referans sayısı)" olarak tuttulur [100,200,300] ise heapte "102 (referans sayısı)"olarak tuttulur.
+            //new heap te yeni adres oluştur demek.
+            //sayilar1 = sayilar2 satırı sayilar1 in referans sayısı sayilar2 nin referans sayısına eşittir olarak okunur.
+            //sayilar2[0] = 999; kodu sayılar2'nin referans numarasının (102) 0. elamanı 999 a eşittir olarak okunur.
+            //bu da sayilar1 in ref numarası artık 102 olduğundan sayilar1 in 0. elamanı 999 olur.
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "amasya elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------");
+            }
+            
+            Console.WriteLine("------------------METOTLAR-----------------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("armut", "yesil armut", 12, 10);
+            sepetManager.Ekle2("elma", "yesil elma", 12, 9);
+            sepetManager.Ekle2("karpuz", "Diyarbakır karpuzu", 12, 8);
+
+        }
+    }
+}
+//metotlar tekrar tekrar kullanım / örn her üründe her sayfada sepete ekle çıkması... yazılan kodlar class lar içerisinde olur.
